5e7875117a10e7f2509c7d2082f6caf6c09f8dfb
jcruzba <114691251+jcruzba@users.noreply.github.com>
2022-10-27 20:10:28 -0300

    Controle de fluxo usando switch, case, break and default.

--- src/Main.java
+++ src/Main.java
@@ -12,36 +12,19 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int mes = 13;
-        {
-            if (mes == 1) {
-                System.out.println("janeiro ");
-            } else if (mes == 2) {
-                System.out.println("fevereiro ");
-            } else if (mes == 3) {
-                System.out.println("março ");
-            } else if (mes == 4) {
-                System.out.println("abril ");
-            } else if (mes == 5) {
-                System.out.println("maio ");
-            } else if (mes == 6) {
-                System.out.println("junho ");
-            } else if (mes == 7) {
-                System.out.println("julho ");
-            } else if (mes == 8) {
-                System.out.println("agosto ");
-            } else if (mes == 9) {
-                System.out.println("setembro ");
-            } else if (mes == 10) {
-                System.out.println("outubro ");
-            } else if (mes == 11) {
-                System.out.println("novembro ");
-            } else if (mes == 12) {
-                System.out.println("dezembro ");
-            } else System.out.println("Mês indefinido");
+        String mes = "janeiro";
+            switch (mes) {
+                case "dezembro":
+                case "julho":
+                case "agosto":
+                    System.out.println("férias");
+                    break;
+                default:
+                    System.out.println("mês indefinido");
+                    break;
+            }
         }
     }
-}
 
 
 
