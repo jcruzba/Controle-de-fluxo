b694734eafeb0a211fa3d88fac57421a6353f880
jcruzba <114691251+jcruzba@users.noreply.github.com>
2022-10-27 19:58:04 -0300

    Controle de fluxo usando if else

---
+++ src/Main.java
@@ -0,0 +1,133 @@
+
+
+/**
+ * Classe de exemplo para o exercicio da aula de
+ * controle de fluxo.
+ * Criar um simples projeto usando if e switch. Com if exiba o nome do mês do ano de acordo
+ * com seu numero. Evite o efeito "flecha". Faça também  outro if  que verifique se o mês é julho,
+ * dezembro ou janeiro, para assim exibir o texto "férias".
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int mes = 13;
+        {
+            if (mes == 1) {
+                System.out.println("janeiro ");
+            } else if (mes == 2) {
+                System.out.println("fevereiro ");
+            } else if (mes == 3) {
+                System.out.println("março ");
+            } else if (mes == 4) {
+                System.out.println("abril ");
+            } else if (mes == 5) {
+                System.out.println("maio ");
+            } else if (mes == 6) {
+                System.out.println("junho ");
+            } else if (mes == 7) {
+                System.out.println("julho ");
+            } else if (mes == 8) {
+                System.out.println("agosto ");
+            } else if (mes == 9) {
+                System.out.println("setembro ");
+            } else if (mes == 10) {
+                System.out.println("outubro ");
+            } else if (mes == 11) {
+                System.out.println("novembro ");
+            } else if (mes == 12) {
+                System.out.println("dezembro ");
+            } else System.out.println("Mês indefinido");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** private static void switchSemana() {
+ String dia = "Terça";
+ switch (dia){
+
+ case "segunda";
+
+ System.out.println("1");
+
+ break;
+
+ case "terça";
+
+ System.out.println("2");
+
+ break;
+
+ case "quarta";
+
+ System.out.println("3");
+
+ break;
+
+ case "quintao";
+
+ System.out.println("4");
+
+ break;
+
+ case "sextao";
+
+ System.out.println("5");
+
+ break;
+
+ case "sábado";
+
+ System.out.println("6");
+
+ break;
+
+ case "domingo";
+
+ System.out.println("7");
+
+ break;
+
+ default;
+
+
+ System.out.println("7");
+ System.out.println("Dia inválido");
+ break;
+
+
+ }
+
+
+ }
+ }
+
+ */
+
